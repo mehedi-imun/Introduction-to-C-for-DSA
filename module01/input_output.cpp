5673d2515b15fec32f84967c7c6013c4ec936b14
mahedi <mahedianik4@gmail.com>
2023-07-22 18:50:12 +0600

    introdruction to c++

---
+++ module01/input_output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    std::cout<<"hello world";
+    std::cout<<"hello"<<std::endl<<"world";
+    int a=100,b=80;
+    std::cout<<a;
+    cout<<a<<endl<<b;
+    return 0;
+}
